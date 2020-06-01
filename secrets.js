c7f2cab1f8cb5cae7f3a480c3be68f7c7068d081
Andrew Hearse <60844536+bluekraken@users.noreply.github.com>
2020-06-01 17:55:19 +0100

    Delete secrets.js

--- secrets.js
+++
@@ -1,9 +0,0 @@
-const infuraProjectId = "1c33604d37754bd1b3534b43c5d930b2";
-const accountPK = "0c68e162b53f4e5d94c76b604407e856";
-const mainnetPK = accountPK;
-const ropstenPK = accountPK;
-module.exports = {
-    infuraProjectId: infuraProjectId,
-    mainnetPK: mainnetPK,
-    ropstenPK: ropstenPK
-}
