0cefacbfde15f34b6ed4f276bdbad261f9a3792a
Andrew Hearse <andy@claroh.com>
2020-06-01 17:53:04 +0100

    Added a new file

---
+++ secrets.js
@@ -0,0 +1,9 @@
+const infuraProjectId = "1c33604d37754bd1b3534b43c5d930b2";
+const accountPK = "0c68e162b53f4e5d94c76b604407e856";
+const mainnetPK = accountPK;
+const ropstenPK = accountPK;
+module.exports = {
+    infuraProjectId: infuraProjectId,
+    mainnetPK: mainnetPK,
+    ropstenPK: ropstenPK
+}
